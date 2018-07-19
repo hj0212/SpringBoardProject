ba9fe53bb1dbbbe9c6c22d4f0ca8fc18023f7395
Unknown <hyejin0212@gmail.com>
2018-07-19 14:48:03 +0900

    board dto, dao, service 생성

--- src/main/java/kh/spring/controller/BoardController.java
+++ src/main/java/kh/spring/controller/BoardController.java
@@ -1,19 +1,20 @@
 package kh.spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import kh.spring.interfaces.IBoardService;
+
 @Controller
 public class BoardController {
 	
-	
+	@Autowired
+	private IBoardService service;
 	
 	@RequestMapping("/boardlist.bo")
-	public ModelAndView getBoardData() {
-		
-		
-		
+	public ModelAndView goBoardList() {
 		
 		return null;
 	}
