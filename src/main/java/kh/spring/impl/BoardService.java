aec0f6ed03dd8957d47b081fda5ec7764b395da1
Unknown <hyejin0212@gmail.com>
2018-07-19 15:30:11 +0900

    boardlist

--- src/main/java/kh/spring/impl/BoardService.java
+++ src/main/java/kh/spring/impl/BoardService.java
@@ -3,11 +3,13 @@ package kh.spring.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import kh.spring.dto.BoardDTO;
 import kh.spring.interfaces.IBoardDAO;
 import kh.spring.interfaces.IBoardService;
 
+@Component
 public class BoardService implements IBoardService {
 	
 	@Autowired
--- src/main/java/kh/spring/interfaces/IBoardService.java
+++ src/main/java/kh/spring/interfaces/IBoardService.java
@@ -2,11 +2,8 @@ package kh.spring.interfaces;
 
 import java.util.List;
 
-import org.springframework.stereotype.Component;
-
 import kh.spring.dto.BoardDTO;
 
-@Component
 public interface IBoardService {
 	public List<BoardDTO> getBoardData();
 }
